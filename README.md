be6a858fd5a2170b6f42c6f4f213165392b2f400
Prabhjot Singh <prabhjot@webmobsoft.com>
2023-06-26 13:39:11 +0530

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# IMFEMain
+# MainApp
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.5.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.3.
 
 ## Development server
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const URL = 'http://localhost:3000/remoteEntry.js';
+
+export const APP_ROUTES: Routes = [{
+  path: 'chat',
+  loadChildren: () =>
+    loadRemoteModule({
+      type: 'manifest',
+      remoteName: 'chat',
+      exposedModule: './Module',
+    }).then((m) => m.ChatModule),
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(APP_ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

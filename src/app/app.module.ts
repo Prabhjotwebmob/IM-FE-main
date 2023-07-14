dcfc6bab2f669b979bb32146f3756baf13144383
Prabhjot Singh <prabhjot@webmobsoft.com>
2023-07-14 12:35:39 +0530

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 import { MainComponent } from './main/main.component';
 import { StoreModule } from '@ngrx/store';
+import { SharedModule } from '@libs/shared-module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -12,10 +13,11 @@ import { StoreModule } from '@ngrx/store';
     MainComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}),
-    HttpClientModule
+    SharedModule,
+    StoreModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
